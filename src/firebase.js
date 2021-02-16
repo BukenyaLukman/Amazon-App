de03923e867213c7594dd32df36044602aa5238a
BukenyaLukman <bukenyalukman@gmail.com>
2021-02-16 18:32:15 +0300

    Amazon App clone two

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBVtHB_hqqOY6dOjG3Kz37vCr_jzsXEta8",
+    authDomain: "clone-b45b4.firebaseapp.com",
+    databaseURL: "https://clone-b45b4.firebaseio.com",
+    projectId: "clone-b45b4",
+    storageBucket: "clone-b45b4.appspot.com",
+    messagingSenderId: "1013363294362",
+    appId: "1:1013363294362:web:bd2d6e9ddf74d4d2ec1f23",
+    measurementId: "G-2E6MVN858W"
+};
